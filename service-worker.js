8c321b608968dd60a2f8c99521e8a0d1d7e28ee9
Saajith2004 <s0279273@gmail.com>
2025-11-21 23:03:44 +0530

    UU

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,17 @@
-const CACHE_NAME = "invoice-cache-v2";
+const CACHE_NAME = "invoice-cache-v3";
 
 const ASSETS = [
-  "invoice_app.html",
-  "manifest.json",
-  "service-worker.js",
-  "logo.png"
+  "/Invoice-App-2/invoice_app.html",
+  "/Invoice-App-2/manifest.json",
+  "/Invoice-App-2/service-worker.js",
+  "/Invoice-App-2/invoice-icon-192.png",
+  "/Invoice-App-2/invoice-icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
